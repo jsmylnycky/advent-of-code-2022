83d87e28d7d4c6b28a44ecaa384192fa80646efb
Jason Smylnycky <jason@litteraeducation.com>
2022-12-05 08:42:34 -0500

    day 2

---
+++ 02/part2.mjs
@@ -0,0 +1,47 @@
+import { open } from 'node:fs/promises'
+
+const file = await open('./input.txt')
+
+const ROCK     = 1
+const PAPER    = 2
+const SCISSORS = 3
+
+const LOST = 0
+const DRAW = 3
+const WON = 6
+
+// X = LOST
+// Y = DRAW
+// Z = WON
+
+const inputs = {
+  'A': { // Opponent Rock
+    'X': SCISSORS + LOST,
+    'Y': ROCK + DRAW,
+    'Z': PAPER + WON
+  },
+  'B': { // Opponent Paper
+    'X': ROCK + LOST,
+    'Y': PAPER + DRAW,
+    'Z': SCISSORS + WON
+  },
+  'C': { // Opponent Scissors
+    'X': PAPER + LOST,
+    'Y': SCISSORS + DRAW,
+    'Z': ROCK + WON
+  }
+}
+
+let totalScore = 0
+
+for await (const line of file.readLines()) {
+  const roundInputs = line.split(' ')
+
+  totalScore += inputs[roundInputs[0]][roundInputs[1]]
+}
+
+// Part One
+console.log('What would your total score be if everything goes exactly according to your strategy guide?')
+console.log(totalScore)
+
+console.log()
